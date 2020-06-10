3e5a6b4fa2da1aa52ffa558328381a401abe6f5f
unknown <JoJo@DESKTOP-A0430JU.GCBLTD.COM>
2020-06-10 04:38:21 +0000

    gitignore

--- src/components/addItem.js
+++ src/components/addItem.js
@@ -23,7 +23,7 @@ const AddItem = () => {
   return (
     <div>
       <h4 className='text-center mb-3' style={{ color: 'rgb(199, 125, 15)' }}>
-        Add Item
+        Add Item (remove this after)
       </h4>
       <form onSubmit={add}>
         <div className='form-group'>
